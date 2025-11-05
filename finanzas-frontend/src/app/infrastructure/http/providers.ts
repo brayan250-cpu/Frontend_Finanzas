ed571eaad2c08f1791ad6949d6ecacb9bb4ee155
Brayan Denis <brayanguizado5@gmail.com>
2025-11-04 19:20:05 -0500

    fix(di): bind CLIENT_REPOSITORY to ClientHttpRepository in infrastructure providers

--- finanzas-frontend/src/app/infrastructure/http/providers.ts
+++ finanzas-frontend/src/app/infrastructure/http/providers.ts
@@ -1,7 +1,6 @@
 import { Provider } from '@angular/core';
-import { ClientRepository } from '../../application/ports/client.repository';
 import { ClientHttpRepository } from './client.http.repository';
-import { CLIENT_REPOSITORY } from '../../application/use-cases/get-all-clients.usecase';
+import { CLIENT_REPOSITORY } from '../../application/tokens';
 
 export const INFRASTRUCTURE_PROVIDERS: Provider[] = [
   { provide: CLIENT_REPOSITORY, useExisting: ClientHttpRepository },
